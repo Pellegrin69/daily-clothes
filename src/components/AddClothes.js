f1599cc6dfa8433502b5f50600e82c833f50193f
pellegrin.pierre <pierre.pellegrin@esqese.ucly.fr>
2022-01-12 11:15:50 +0100

    Rangement de dossier

--- src/components/AddClothes.js
+++ src/components/AddClothes.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import {useEffect, useState} from "react";
-import {addAClothe, deleteAClothe, getAllClothes, updateAClothe} from "../../api/clothes";
+import {addAClothe, deleteAClothe, getAllClothes, updateAClothe} from "../api/clothes";
 import "./AddClothes.css"
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
