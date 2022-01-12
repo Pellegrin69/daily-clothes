3c0da0651dfe3889c71b6a3ec49cc9cde1413a6c
Zekirr <59977659+Zekirr@users.noreply.github.com>
2022-01-12 11:39:19 +0100

    commit page profile avec bouton suppression de compte

--- src/api/user.js
+++ src/api/user.js
@@ -1,35 +1,50 @@
 const url = 'http://localhost:8000/users';
 const headers = {
-  'Content-Type': 'application/json',
-  'Accept': 'application/json'
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
 };
 
 export const getAllUsers = () => {
 
-  return fetch(url, {headers})
-    .then((response) => response.json())
-    .catch((error) => console.log(error))
+    return fetch(url, {headers})
+        .then((response) => response.json())
+        .catch((error) => console.log(error))
 }
 
 export const getOneUser = (userId) => {
-  return fetch(`${url}/${userId}`, {method: 'GET', headers})
-    .then((response) => response.json())
+    return fetch(`${url}/${userId}`, {method: 'GET', headers})
+        .then((response) => response.json())
 }
 
 export const createAUsers = (newUser) => {
-  return fetch(url, {method: 'POST', headers, body: JSON.stringify(newUser)})
+    return fetch(url, {method: 'POST', headers, body: JSON.stringify(newUser)})
 }
 
 export const updateAUsers = (updatedUser) => {
-  return fetch(`${url}/${updatedUser.id}`, {method: 'PUT', headers, body: JSON.stringify(updatedUser)})
+    return fetch(`${url}/${updatedUser.id}`, {method: 'PUT', headers, body: JSON.stringify(updatedUser)})
 }
 
 export const deleteAUsers = (deletedUserID) => {
-  return fetch(`${url}/${deletedUserID}`, {method: 'DELETE', headers})
+    return fetch(`${url}/${deletedUserID}`, {method: 'DELETE', headers})
 }
 
 export const removeUserSession = (navigate) => {
     localStorage.removeItem("user");
     localStorage.removeItem("token");
     navigate("/signin")
+}
+
+export const getUser = () => {
+    const userStr = localStorage.getItem("user");
+    if (userStr) return JSON.parse(userStr)
+    else return null
+}
+
+export const getToken = () => {
+    return localStorage.getItem("token") || null;
+}
+
+export const setUserSession = (user, token) => {
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("token", token);
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
-import {getOneUser} from "../api/user";
+import {getOneUser, getUser} from "../api/user";
 import {removeUserSession} from  "../api/user.js";
+import {deleteAUsers} from  "../api/user.js";
 import {useNavigate} from "react-router";
 
 const Profile = () => {
   const {userId} = useParams();
-  const [user, setUser] = useState(null)
+  const [setUser] = useState(null)
   const navigate = useNavigate();
+  const user = getUser();
 
   useEffect(() => {
     getOneUser(userId)
@@ -15,6 +17,9 @@ const Profile = () => {
   }, [])
   const logout = () => {
     removeUserSession(navigate)
+  }
+  const deleteuser = () => {
+    deleteAUsers(user.id)
   }
   return <>
     <h2>Profile</h2>
@@ -38,6 +43,7 @@ const Profile = () => {
           </div>
         </div>
           <button onClick={logout} className="btn btn-danger">Log out</button>
+          <button onClick={deleteuser} className="btn btn-danger">Delete the account</button>
       </>
     }
   </>
